0ca8347a25b7ce827a6b9a9e15ab4727b3cfce32
Malte <70883688+MalteZea@users.noreply.github.com>
2020-12-22 11:30:00 +0100

    Edit

---
+++ src/sample/Ingrediens.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Ingrediens {
+    private String navn;
+    private int kalorie;
+
+    private Ingrediens(){
+        System.out.println("Ingrediens er oprettet");
+    }
+}
